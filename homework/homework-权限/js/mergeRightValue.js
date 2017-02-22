95538438c78b97d7d000be1448038678a8b944cd
LvChunTing <15063043893@163.com>
2017-02-22 09:23:41 +0800

    作业提交

---
+++ homework/homework-权限/js/mergeRightValue.js
@@ -0,0 +1,20 @@
+var personPower=["1000","11111100","1001010100"]
+function mergeRightValue(personPower) {
+    var Power=[];
+    for(var i=0;i<personPower.length;i++){
+        for(var j=0;j<personPower[i].length;j++){
+            if(Power[j]==undefined){
+                Power[j]="0";
+            }
+            if(Power[j]=="1"||personPower[i][j]=="1"){
+                Power[j] = "1";
+            }else{
+                Power[j] = "0";
+            }
+        }
+
+    }
+    Power=Power.join("");
+    console.log(Power)
+}
+mergeRightValue(personPower)
